b7d0e7f5910da469e10463b8f8708b4607bf54fc
Yuskus <yuskus@yandex.ru>
2024-01-24 14:24:31 +0300

    first commit

---
+++ Flags.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Flags : MonoBehaviour
+{
+    public static readonly int Code_Ketchup = 1;
+    public static readonly int Code_Mustard = 2;
+    public static readonly int Code_Onion = 4;
+    public static readonly int Code_Doneness = 8;
+    public static readonly int Code_Burger = 16;
+    public static readonly int Code_HotDog = 32;
+}
