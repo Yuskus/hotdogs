fda1b94f38e152f22f597c218d2ad9984802f56d
Yuskus <yuskus@yandex.ru>
2024-01-24 14:18:08 +0300

    first commit

---
+++ StartBurger.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class StartBurger : MonoBehaviour, IPointerDownHandler
+{
+    private Drag dg;
+    private GameObject StolBurger;
+    private void Awake()
+    {
+        if (RecData.ContinueGame < RecData.canCookBurger) { transform.gameObject.SetActive(false); }
+    }
+    private void Start()
+    {
+        dg = Camera.main.GetComponent<Drag>();
+        StolBurger = transform.parent.GetChild(4).gameObject;
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (!dg.isDragging)
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                if (!StolBurger.transform.GetChild(i).gameObject.activeInHierarchy) { StolBurger.transform.GetChild(i).gameObject.SetActive(true); break; }
+            }
+        }
+    }
+}
