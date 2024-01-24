6ffa6de48a3d996801f98649e9eb8511a95007d6
Yuskus <yuskus@yandex.ru>
2024-01-24 14:17:22 +0300

    first commit

---
+++ StartKotleta.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class StartKotleta : MonoBehaviour, IPointerDownHandler
+{
+    private Drag dg;
+    private GameObject PlitaKotlet;
+    private void Awake()
+    {
+        if (RecData.ContinueGame < RecData.canCookBurger) { transform.gameObject.SetActive(false); }
+    }
+    private void Start()
+    {
+        dg = Camera.main.GetComponent<Drag>();
+        PlitaKotlet = transform.parent.GetChild(2).gameObject;
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (!dg.isDragging)
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                if (!PlitaKotlet.transform.GetChild(i).gameObject.activeInHierarchy) { PlitaKotlet.transform.GetChild(i).gameObject.SetActive(true); break; }
+            }
+        }
+    }
+}
